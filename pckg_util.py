e8753b1f2faea0e3e279744cfeeb3dc025f26b45
Semyon <semyonsever@ya.ru>
2024-08-07 11:17:47 +0300

    убрал зависимости, несовместимые с ру3.11

--- pckg_util.py
+++ pckg_util.py
@@ -69,25 +69,5 @@ def check_gpu_and_torch_compatibility():
                     import torch
                 except Exception as e:
                     install_and_import("torch")
-
-
-            if "NVIDIA A100" in output:
-                install_and_import(
-                    "torch",
-                    "1.11.0+cu113",
-                    "-f",
-                    "https://download.pytorch.org/whl/torch_stable.html",
-                )
-                install_and_import(
-                    "torchvision",
-                    "0.12.0+cu113",
-                    "-f",
-                    "https://download.pytorch.org/whl/torch_stable.html",
-                )
-            else:
-                try:
-                    import torch
-                except Exception as e:
-                    install_and_import("torch")
     except OSError as e:
         logging.info("GPU device is not available")
